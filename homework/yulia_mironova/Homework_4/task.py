dfb40fbbe93ce8aec31ed4ce838513803c92c6db
Julia Mironova <yulmiro@mail.ru>
2024-11-26 23:23:26 +0300

    Homework_4_v1

---
+++ homework/yulia_mironova/Homework_4/task.py
@@ -0,0 +1,10 @@
+my_dict = {'tuple': (1, 5, 'text', 100.5, True), 'list': [2, 4, 7, 'yes', 75.9],
+           'dict': {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five'}, 'set': {'lol', 10, False, 'boo', 35.7}}
+print(my_dict['tuple'][-1])
+my_dict['list'].append('new')
+my_dict['list'].pop(1)
+my_dict['dict'] ['i am a tuple'] = 'six'
+del my_dict ['dict'][2]
+my_dict['set'].add('new_value')
+my_dict['set'].remove('boo')
+print(my_dict)
