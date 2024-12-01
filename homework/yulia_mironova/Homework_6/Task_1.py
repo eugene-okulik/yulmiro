4081f043c6355ed36e980ab584b0ee05c7354d4c
Julia Mironova <yulmiro@mail.ru>
2024-12-01 23:51:47 +0300

    Homework_6_v1

---
+++ homework/yulia_mironova/Homework_6/Task_1.py
@@ -0,0 +1,13 @@
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. " \
+        "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+marks = ['.', ',', '?', '!', ';', ':', '-', '...']
+list_text = text.split()
+new_text = ''
+for word in list_text:
+    if word[-1] in marks:
+        baza = word[:-1]
+        mark = word[-1]
+        new_text = new_text + baza + 'ing' + mark + ' '
+    else:
+        new_text = new_text + word + 'ing' + ' '
+print(new_text)
