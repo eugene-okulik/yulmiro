8910d7b2b793559f623ad37f13ebab2ead9bdc75
Julia Mironova <yulmiro@mail.ru>
2024-12-03 22:43:09 +0300

    Homework_7_v1

---
+++ homework/yulia_mironova/Homework_7/Task_1.py
@@ -0,0 +1,6 @@
+number = 15
+user_input = int(input('your number '))
+while number != user_input:
+    print('попробуйте снова')
+    user_input = int(input('your number '))
+print('Поздравляю! Вы угадали!')
---
+++ homework/yulia_mironova/Homework_7/Task_2.py
@@ -0,0 +1,8 @@
+def print_dict(y, x):
+    for i in range(0, x):
+        print(y,end = '')
+    print()
+
+words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
+for key, value in words.items():
+    print_dict(key, value)
