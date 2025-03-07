3506183c73589fcf7db68f3fd473c91846e195f6
Julia Mironova <yulmiro@mail.ru>
2025-03-07 16:51:46 +0300

    Homework_26_v2

--- homework/yulia_mironova/Homework_26/playwright_basic.py
+++ homework/yulia_mironova/Homework_26/playwright_basic.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page
 
+
 def test_get_by_role(page: Page):
     page.goto('https://the-internet.herokuapp.com/')
     page.get_by_role('link', name='Form Authentication').click()
