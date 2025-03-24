7a722fb78cf1a5d67eff1912e9f4e5b256f243a3
Julia Mironova <yulmiro@mail.ru>
2025-03-25 00:20:06 +0300

    Homework_28_v2

--- homework/yulia_mironova/Homework_28/playwright_3.py
+++ homework/yulia_mironova/Homework_28/playwright_3.py
@@ -8,14 +8,12 @@ def test_rewrite_header(page: Page):
     def rewrite_apple(route: Route):
         response = route.fetch()
         body = response.json()
-        body["body"]["digitalMat"][0]["familyTypes"][0]["productName"]= "яблокофон 16 про"
+        body["body"]["digitalMat"][0]["familyTypes"][0]["productName"] = "яблокофон 16 про"
         body = json.dumps(body)
         route.fulfill(
             response=response,
             body=body
         )
-
-
     page.route(re.compile('step0_iphone'), rewrite_apple)
     page.goto('https://www.apple.com/shop/buy-iphone')
     cards = page.locator('[class="rf-hcard-content-title"]')
